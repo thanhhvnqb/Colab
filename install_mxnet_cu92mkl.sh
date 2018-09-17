ace77889b32379a29c53ae1c3234050ce4ffd2d2
thanhhvnqb <thanhhvnqb@gmail.com>
2018-09-17 13:15:28 +0900

    Update install_mxnet_cu92mkl.sh

--- install_mxnet_cu92mkl.sh
+++ install_mxnet_cu92mkl.sh
@@ -4,14 +4,14 @@ TEXT_RESET='\e[0m'
 TEXT_YELLOW='\e[1;33m'
 
 wget https://raw.githubusercontent.com/thanhhvnqb/Mxnet_colab/master/mxnet_utils.py -O mxnet_utils.py
-wget https://developer.nvidia.com/compute/cuda/9.2/Prod2/local_installers/cuda-repo-ubuntu1710-9-2-local_9.2.148-1_amd64
+wget https://developer.nvidia.com/compute/cuda/9.2/Prod2/local_installers/cuda-repo-ubuntu1604-9-2-local_9.2.148-1_amd64
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 echo -e $TEXT_YELLOW
 echo 'WEBGET finished..'
 echo -e $TEXT_RESET
 
-dpkg -i cuda-repo-ubuntu1710-9-2-local_9.2.148-1_amd64
+dpkg -i cuda-repo-ubuntu1604-9-2-local_9.2.148-1_amd64
 echo -e $TEXT_YELLOW
 echo 'DPKG finished..'
 echo -e $TEXT_RESET
